5a70d38301f46103b74edf40546a1313cb04d8f6
AungNyanHtet18 <aungnyanh81@gmail.com>
2026-03-23 02:26:28 +0630

    admin sign in layout

--- jwt-security-web/app/(anonymous)/layout.tsx
+++ jwt-security-web/app/(anonymous)/layout.tsx
@@ -1,8 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { Home, Shield } from "lucide-react";
+import Link from "next/link";
+
 export default function AnnonymousLayout({children} : {children: React.ReactNode}) {
      return (
-        <div>
-            <h1>Annonymous Page</h1>
-            {children}
+        <div className="h-screen flex">
+             <header  className="flex-1 bg-zinc-600 text-white flex flex-col  justify-center items-center gap-4 ">
+                 <Shield className="size-48"/>
+                 <h1 className="text-3xl fw-bold uppercase">Security Demo</h1>
+                 <Button asChild className="bg-white text-black">
+                    <Link href={'/'}>
+                        <Home/> Home
+                    </Link>
+                 </Button>
+             </header>
+
+             <main className="flex-1 flex justify-center items-center">
+                <section className="w-1/2">
+                    {children}
+                </section>
+             </main>
         </div>
      )
 }
--- jwt-security-web/app/(anonymous)/signin/page.tsx
+++ jwt-security-web/app/(anonymous)/signin/page.tsx
@@ -1,7 +1,48 @@
+'use client'
+
+import FormsInput from "@/components/forms/forms-input"
+import { Button } from "@/components/ui/button"
+import { SignInForm, SignInFormSchema } from "@/lib/form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { LogIn, Unlock, UnlockIcon } from "lucide-react"
+import { useRouter } from "next/navigation"
+import { useForm } from "react-hook-form"
+
 export default function SignInPage() {
-     return (
+
+    const router = useRouter()
+
+    const form = useForm<SignInForm>({ 
+        resolver: zodResolver(SignInFormSchema),
+        defaultValues: {
+           email: '',
+           password: ''
+        }
+    })
+
+    const signInAction = (form: SignInForm) => {
+        const routerUrl = `/${form.password.toLocaleLowerCase()}`
+        router.replace(routerUrl)
+    }
+
+    return (
         <div>
-            <h1>Sign In Page</h1>
+            <h1 className="flex items-center gap-2">
+                <LogIn/> <span className="text-2xl">Sign In</span>
+            </h1>
+
+            <form onSubmit={form.handleSubmit(signInAction)} className="space-y-4">
+                <FormsInput controls={form.control} path="email" label="Login ID" />
+                <FormsInput controls={form.control} path="password" label="Password" />
+
+                <nav>
+                    <Button type="submit">
+                        <Unlock/> Sign In
+                    </Button>
+                </nav>
+
+            </form>
+
         </div>
      )
 }
